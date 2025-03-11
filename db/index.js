d9c39f4b7d0b03dfda29fccd4cb685b095d50eaa
Mounika-Nanjala <nanjalamounika9@gmail.com>
2025-03-11 12:48:58 +0100

    New Order

--- db/index.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('postgresql://ecommerce-api_owner:npg_pD9U2ihjqayb@ep-sweet-wind-a2k765br-pooler.eu-central-1.aws.neon.tech/ecommerce-api?sslmode=require', {
-  dialect: 'postgres',
-  logging: false,
-});
-
-export default sequelize;
